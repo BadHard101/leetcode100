bb835f054d27604bf0b9a15b8b5e3f9b8f5a01ae
BadHard <BadHard101@yandex.ru>
2024-01-09 20:03:39 +0300

    initial

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        return true;
+        if (p != null && q != null) {
+            if (p.val == q.val) {
+                return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+            }
+        } else return p == null && q == null;
+        return false;
     }
     public static void main(String[] args) {
         System.out.println("Hello world!");
