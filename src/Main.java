fb8d7819bcb720d14a11913ab7434b79d7163296
BadHard <BadHard101@yandex.ru>
2024-01-09 12:34:14 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        return true;
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
